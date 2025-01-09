c8d96e52e5d87124b485465d938eaf30a74a11e4
Dev-with-Osm <khourssaoussama7@gmail.com>
2025-01-10 00:14:18 +0100

    implement error handling and command execution in pipex

--- pipex.c
+++ pipex.c
@@ -1,92 +1,126 @@
 #include "pipex.h"
 
+void error_check(char *errType, char *errMsg)
+{
+	fprintf(stderr, "%s %s\n", errType, errMsg);
+	exit(EXIT_FAILURE);
+}
+
+void err_return(void)
+{
+	perror("Error");
+	exit(EXIT_FAILURE);
+}
+
+void ft_putstr(char *s, int fd)
+{
+	if (!s)
+		return;
+	write(fd, s, strlen(s));
+}
+
+void print_error(char *text, char *type, char *cmd)
+{
+	ft_putstr(type, 2);
+	ft_putstr(": ", 2);
+	ft_putstr(text, 2);
+	if (cmd)
+		ft_putstr(cmd, 2);
+	ft_putstr("\n", 2);
+}
+
+int open_file(char *filePath, int flag)
+{
+	int fd;
+
+	if (flag == 1)
+		fd = open(filePath, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (flag == 2)
+		fd = open(filePath, O_RDONLY);
+	else
+		fd = open(filePath, O_RDWR | O_CREAT, 0644);
+
+	if (fd < 0)
+		error_check("Error", "Failed to open file");
+	return fd;
+}
+
 char *check_cmd(char *cmd, char **env)
 {
 	int i = 0;
-	while (env[i] != NULL)
-	{
-		if (strncmp("PATH=", env[i], 5) == 0)
-			break;
+
+	while (env[i] && strncmp("PATH=", env[i], 5) != 0)
 		i++;
-	}
-	if (env[i] == NULL)
-	{
-		printf("fghjk");
-		exit(1);
-	}
-	char **res = ft_split(env[i], '=');
-	char **split_res = ft_split(res[1], ':');
+	if (!env[i])
+		return (print_error("command not found: ", "command failed", cmd), NULL);
+	char **paths = ft_split(env[i] + 5, ':');
+	if (!paths)
+		return (error_check("Error", "Failed to allocate memory"), NULL);
+	char *full_cmd = NULL;
 	i = 0;
-	while (split_res[i])
+	while (paths[i])
 	{
-		char *str = ft_strjoin(split_res[i], "/");
-		char *full_cmd = ft_strjoin(str, cmd);
-		if (access(full_cmd, X_OK | F_OK) == 0)
-			return (full_cmd);
+		char *path_with_slash = ft_strjoin(paths[i], "/");
+		full_cmd = ft_strjoin(path_with_slash, cmd);
+		if (access(full_cmd, X_OK) == 0)
+			return full_cmd;
 		i++;
 	}
+	print_error("command not found: ", "command failed", cmd);
 	return NULL;
+}
+
+void execute_cmd(char *cmd_with_args, char **env, int input_fd, int output_fd)
+{
+	char **args = ft_split(cmd_with_args, ' ');
+	if (!args)
+		error_check("Error", "Failed to allocate memory for command arguments");
+	char *cmd = check_cmd(args[0], env);
+	if (!cmd)
+		exit(127);
+	if (dup2(input_fd, STDIN_FILENO) == -1 || dup2(output_fd, STDOUT_FILENO) == -1)
+		error_check("Error", "Failed to duplicate file descriptor");
+	execve(cmd, args, env);
+	perror("execve");
+	exit(127);
 }
 
 int main(int argc, char const *argv[], char *env[])
 {
-
 	if (argc != 5)
-	{
-		printf("Error: Too few arguments\n");
-		exit(1);
-	}
+		error_check("Usage", "./pipex infile \"cmd1\" \"cmd2\" outfile");
+
 	int fd[2];
 	if (pipe(fd) == -1)
-	{
-		perror("err:");
-		exit(1);
-	}
+		err_return();
 
 	pid_t pid1 = fork();
-	pid_t pid2;
 	if (pid1 == -1)
-	{
-		perror("err:");
-		exit(1);
-	}
+		err_return();
+
 	if (pid1 == 0)
-	{
-		// the first child process to handle the first cmd
+
 		close(fd[0]);
-		int file = open(argv[1], O_RDWR);
-		dup2(file, STDIN_FILENO);
-		close(file);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
+	int input_fd = open_file((char *)argv[1], 2);
+	execute_cmd((char *)argv[2], env, input_fd, fd[1]);
+}
 
-		char **args = ft_split(argv[2], ' ');
-		char *cmd = check_cmd(args[0], env);
-		execve(cmd, args, env);
-	}
+pid_t pid2 = fork();
+if (pid2 == -1)
+	err_return();
 
-	pid2 = fork();
-	if (pid2 == -1)
-	{
-		perror("err:");
-		exit(1);
-	}
-	if (pid2 == 0)
-	{
-		waitpid(pid1, NULL, 0);
-		close(fd[1]);
+if (pid2 == 0)
+{
 
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		int file = open(argv[4], O_WRONLY | O_TRUNC);
-		dup2(file, 1);
+	close(fd[1]);
+	int output_fd = open_file((char *)argv[4], 1);
+	execute_cmd((char *)argv[3], env, fd[0], output_fd);
+}
 
-		char **args = ft_split(argv[3], ' ');
-		char *cmd = check_cmd(args[0], env);
-		execve(cmd, args, env);
-	}
+close(fd[0]);
+close(fd[1]);
+waitpid(pid1, NULL, 0);
+waitpid(pid2, NULL, 0);
 
-	wait(NULL);
-
-	return 0;
+return 0;
 }
